14fce88c0546918757cc3c6c9bef5327ffceac0c
Mau Lomba <mauriciolombard@liamdeMacBook-Air.local>
2018-07-28 06:49:48 -0500

    Map loop over robots

--- src/CardList.js
+++ src/CardList.js
@@ -2,10 +2,14 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({robots}) => {
+
     return (
         <div>
-            <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-            <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+            {
+                robots.map((user, i) => {
+                    return  <Card  key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
+                })
+            }
         </div>
     )
 }
