50704cf8c7072e1d8246cc3b793b6d5b383a196f
Mau Lomba <mauriciolombard@liamdeMacBook-Air.local>
2018-07-27 21:03:18 -0500

    pass props to Card component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import Card from './Card';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
+import {robots} from './robots';
 
-ReactDOM.render(<Card/>, document.getElementById('root'));
+ReactDOM.render(<div>
+                    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+                    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+                </div>
+                , document.getElementById('root'));
 registerServiceWorker();
---
+++ src/robots.js
@@ -0,0 +1,14 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'shanna@melissa.tv'
+    }
+]
