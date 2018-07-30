8408eb939680a2ca889774e247aadc5e9fce17f7
Mau Lomba <mauriciolombard@liamdeMacBook-Air.local>
2018-07-30 10:51:24 -0500

    Upgrade App Component from stateless(dumb) to smart

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import {robots} from './robots';
 import SearchBox from './SearchBox';
 
 
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>RoboFriends</h1>
-            <SearchBox/>
-            <CardList robots={robots}/>
-        </div>
-        
-    )
+class App extends Component {
+    constructor (){
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        };
+    }
+    
+    render (){
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox/>
+                <CardList robots={this.state.robots}/>
+            </div>
+        )
+    }
 }
 
 export default App;
