cbc229ff814579a27a013d87e97223287b805aeb
Mau Lomba <mauriciolombard@liamdeMacBook-Air.local>
2018-08-01 17:02:31 -0500

    commenting out console.logs

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,7 @@
 import {CHANGE_SEARCH_FIELD} from './constants';
 
 export const setSearchField = (text) => {
-    console.log(text);
+    // console.log(text);
     return {
         type: CHANGE_SEARCH_FIELD,
         payload: text
