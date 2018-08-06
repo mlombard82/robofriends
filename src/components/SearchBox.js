31338ef3c1e770a922347434b903a3d9a05735d3
Mau Lomba <mauriciolombard@liamdeMacBook-Air.local>
2018-08-06 11:00:55 -0500

    PWA and Accessibility

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({SearchChange}) => {
     return (
         <div className='pa2'>
             <input className='pa3 ba b--green bg-lightest-blue' 
+                    aria-label='Search Robots'
                     onChange={SearchChange}
                     type="search" 
                     placeholder="search"
